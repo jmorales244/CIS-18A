7654abd294ba45c81348853da315c06176ad6139
JheremyM244 <jmorales244@student.rccd.edu>
2022-06-22 10:58:22 -0700

    Uploading First Hello World

---
+++ test/src/main/java/test/Main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+import java.util.Scanner;
+/**
+ *
+ * @author jhere
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {        
+        Scanner scanf = new Scanner(System.in);
+        System.out.print("\t *** Monthly Sales Tax ***\n");
+        System.out.println("Please enter current month and year: ");
+        String Date;
+        Date = scanf.nextLine();
+    }
+    
+}
