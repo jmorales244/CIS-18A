9cd47f4a4a5deb80e728298cfe50a6b7ccebdb03
JheremyM244 <jmorales244@student.rccd.edu>
2022-06-29 11:59:13 -0700

    Uploading Pennies for Pay - Assignment 3

--- Pennies for Pay/src/pennies/pkgfor/pay/PenniesForPay.java
+++ Pennies for Pay/src/pennies/pkgfor/pay/PenniesForPay.java
@@ -1,35 +1,39 @@
-
 package pennies.pkgfor.pay;
 import java.util.*;
 
 /**
  *
- * @author jhere
+ * @author Jheremy Morales
  */
 public class PenniesForPay {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         System.out.print("For how many days will pay double? : ");
         Scanner cin = new Scanner(System.in);
         int input = cin.nextInt();
         double penny = 0.01;
+        double total = 0.01;
         System.out.print("Days\t Total Pay");
         System.out.print("\n--------------------------");
-        System.out.print("\n1 \t$\t" + String.format("%.2f", doubleValue(penny, 1)));
-        for(int i = 2; i < input; i++){
-            System.out.print("\n" + (i) + "\t$\t" + String.format("%.2f", doubleValue(penny, (i + 1))));
-           
-            
+        System.out.print("\n1 \t$\t" + String.format("%.2f", penny));
+        for(int i = 1; i < input; i++){
+            System.out.print("\n" + (i+1) + "\t$\t" + String.format("%.2f", doubleValue(penny, (i)))); 
+            total += doubleValue(penny, (i));
         }
-        
+        System.out.print("\n--------------------------");
+        System.out.print("\nTotal\t$\t" + String.format("%.2f", total) + "\n");
+       
         
     }
+    /**
+     * @param x will be the base of what will be doubled with 
+     * @param y will be the amount of times it will be doubled, essentially the exponent
+     * @return the result that has been doubled 
+     */
     static double doubleValue(double x, int y){
         for(int i = 0; i < y; i++){
-            x = x * 2;
+            x *= 2;
         }
         return x;
     }
